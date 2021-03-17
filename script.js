2eb70bff366b0df98252cb232bc74c283d2adca2
vitabinary <vitaassetbinary@gmail.com>
2021-03-17 17:29:38 +0000

    modified

--- script.js
+++ script.js
@@ -3,17 +3,41 @@ function compute()
     principal = document.getElementById("principal").value;
     rate = document.getElementById("rate").value;
     years = document.getElementById("years").value;
-
     interest = (principal*years*rate/100);
+    
+     var amount = parseInt(principal);
+     var interestAmount = parseFloat(interest);
+
+    total = amount + interestAmount;
+
+   
+
+    document.getElementById('result').innerHTML = interest;
    
     date = new Date()
-
-    year = date.setFullYear()
-
    
+    var currentYear = parseInt(years);
 
-    
+    toyear = date.getFullYear()+currentYear;
+
+    document.getElementById('result1').innerHTML = "If you deposit " + principal + ",";
+
+    document.getElementById('result2').innerHTML = "at an interest rate of " + rate + "%.";
+
+    document.getElementById("result3").innerHTML = "You will receive an amount of " + total + ",";
+
+    document.getElementById('result4').innerHTML = "in the year " + toyear;
 
     
 }
+
+function myFunction(){
+
+    getrate= document.getElementById("rate").value;
+    
+    document.getElementById("show").innerHTML = getrate + " %";
+
+}
+
+
         
